46285487091e2461e333daebe406c670f078b9be
Geordan Krahn <geordankrahn@gmail.com>
2022-11-13 20:39:27 -0700

    Creates framework for a singleton game session

---
+++ ISession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gaming
+{
+    /// <summary> Interface Session </summary>
+    interface ISession
+    {
+        /// <summary> The name of the Game </summary>
+        public string GameName { get; set; }
+
+        /// <summary> Current Session State </summary>
+        public GameSessionState SessionState { get; set; }
+        void Initialize();
+        void GameLoop();
+        void EndGame();
+
+    }
+}
---
+++ Session.cs
@@ -0,0 +1,40 @@
+﻿using Gaming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsAndDice
+{
+    /// <summary> Running Session for the game. Static with while loop to enforce a dirty singleton </summary>
+    public static class Session
+    {
+        public static void RunGameSession(GameSession session)
+        {
+            bool isExiting = false;
+            while (!isExiting)
+            {
+                var state = session.SessionState;
+                switch (state)
+                { 
+                    case GameSessionState.NONE:
+                    case GameSessionState.Error:
+                    case GameSessionState.Quit:
+                    default:
+                        isExiting = true;
+                        break;
+                    case GameSessionState.Initialize:
+                        session.Initialize();
+                        break;
+                    case GameSessionState.Playing:
+                        session.GameLoop();
+                        break;
+                    case GameSessionState.End:
+                        session.EndGame();
+                        break;
+                }
+            }
+        }
+    }
+}
